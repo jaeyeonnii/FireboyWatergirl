ace85b130120b9e14273bab5081821b253628bfc
yoonseo <hello.git.cysyes0403@naver.com>
2022-11-23 12:45:08 +0900

    test

--- fbwg game/src/fbwg_1/Fbwg.java
+++ fbwg game/src/fbwg_1/Fbwg.java
@@ -17,7 +17,7 @@ public class Fbwg  extends JFrame{
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		setResizable(false); //사이즈 변경 막기
 		setLocationRelativeTo(null); //컴퓨터 중앙에 창 뜨기
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //게임창 종료시 프로그램 종료 (중요
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //게임창 종료시 프로그램 종료 (중요)
 		setVisible(true); //화면출력
 		setLayout(null);
 		
