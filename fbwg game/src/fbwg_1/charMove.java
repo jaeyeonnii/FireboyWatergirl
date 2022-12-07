2883930f30e236778b43e9dcd5cec8a57a75ef09
Lee Jae Yeon <wodus7792@naver.com>
2022-12-07 17:16:53 +0900

    캐릭터 파일

--- fbwg game/src/fbwg_1/charMove.java
+++ fbwg game/src/fbwg_1/charMove.java
@@ -1,11 +1,47 @@
 package fbwg_1;
+//캐릭터 이동
 
-import java.awt.*;
+import java.awt.*; 
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
+import javax.swing.JLabel.*;
 import java.util.*;
 
-public class charMove {
+public class charMove extends JLabel {
 
+	private int x;
+	private int y;
+	
+	private ImageIcon play1; //기본 캐릭(불)
+	private ImageIcon play2;//기본 캐릭(물)
+	private ImageIcon jump1; //점프 모션(불)
+	private ImageIcon jump2; //점프 모션(물)
+	private ImageIcon dead1; //죽음 모션(물)
+	private ImageIcon dead2; //죽음 모션(불)
+	
+	
+	public charMove() {
+		initObject();
+		initSetting();
+	}
+	
+	private void initObject() {
+		play1= new ImageIcon("");
+		play2= new ImageIcon("");
+		jump1= new ImageIcon("");
+		jump2= new ImageIcon("");
+		dead1= new ImageIcon("");
+		dead2= new ImageIcon("");
+		
+	}
+	private void initSetting() {
+		x = 55;
+		y = 535;
+		
+		this.setIcon(play1);
+		this.setSize(50,50);
+		this.setLocation(x, y);
+		
+		
+	}
 }
