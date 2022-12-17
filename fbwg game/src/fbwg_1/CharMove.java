f07c409fd1658bd13fa8e022434443c09913ea32
Lee Jae Yeon <wodus7792@naver.com>
2022-12-17 19:52:36 +0900

    맵 구성 완료 및 움직이는 벽 올라가기 수정완료!!

--- fbwg game/src/fbwg_1/CharMove.java
+++ fbwg game/src/fbwg_1/CharMove.java
@@ -27,6 +27,8 @@ public class CharMove {
     private boolean down;
     private final int SPEED = 10;
     private final int JUMPSPEED = 3;
+    private int JUMPDOWN = 100;
+    
     int i =0;
     
     
@@ -97,24 +99,64 @@ public class CharMove {
 		return waterRightMove1;
 	}
 	
-	//꼼수파일><
-	public void map() {
-		
+	
+	public void back() {
+		//워터걸 시작 부분 내려오기
 		if(pos_Y==540&&pos_X>=290&&pos_X<=300) {
 			down2();
 		}
-		if(pos_Y<=310&&pos_X>=498) down3();
+		
+		if((pos_Y<=320&&pos_Y>=308&&pos_X<=498&&pos_X>=460)
+			||(pos_Y>=25&&pos_Y<=27&&pos_X<350&pos_X>=295)
+			||(pos_Y>=48&&pos_Y<=52&&pos_X<=360&pos_X>=350)) {
+			
+			JUMPDOWN = 25;
+			down2();
+		}
+		if((pos_Y>=275&&pos_Y<=280&&pos_X<=850&&pos_X>498
+			||(pos_Y<=155&&pos_Y>=153&&pos_X<=475&&pos_X>465)
+			||(pos_Y>=150&&pos_Y<=152&&pos_X>=180&&pos_X<=182))
+			||(pos_Y>=100&&pos_Y<=102&&pos_X>=120&&pos_X<=122)) {
+			JUMPDOWN = 50;
+			down2();
+		}
+		if(pos_Y>=205&&pos_Y<=208&&pos_X>=823&&pos_X<=910) {
+			JUMPDOWN = 70;
+			down2();
+		}
+//		if((pos_X<150&&pos_Y>=435-45&&pos_Y<=435-45+2&&Block.getpush()==false)) {
+//			while(true) {
+//				pos_Y=-2;
+//				
+//				if(pos_Y==310) break;
+//			}
+//		}
 		
 		
 	}
+	public void moveWall() {
+		
+		new Thread(()->{
+			while(true) {
+				if(pos_X<150&&pos_Y>310&&pos_Y<=435-45+2&&Block.getpush()==false)
+					pos_Y-=2;
 
+				try {
+					Thread.sleep(4000);
+				} catch(Exception e) {
+				
+				}
+			}
+		}).start();
+		
+	}
 	public void moveFireRight() {
 		right = true;
 		
 		new Thread(()->{
 			while(pos_X<910&&right) {//!(pos_Y>=540&&pos_Y<=640&&pos_X>=830)
-				map();
-				//block();
+				back();
+				moveWall();
 				
 				if (direction.equals("Fleft")) {
 					direction = "Fright";
@@ -138,16 +180,14 @@ public class CharMove {
 			}
 		}).start();
 		
-		
-		
 	}
 	
 	public void moveFireLeft() {
 		left = true;
 		new Thread(()->{
 			while(pos_X>10&left) {
-				map();
-				//block();
+				back();
+				moveWall();
 				if (direction.equals("Fright")) {
 					direction = "Fleft";
 				}
@@ -170,13 +210,14 @@ public class CharMove {
 		}).start();
 		
 	}
+	
 	public void moveWaterRight() {
 		right = true;
 		new Thread(()->{
 			
 			while(pos_X<910&&right) {//!(pos_Y>=580&&pos_Y<=640&&pos_X>=830)
-				map();
-				//block();
+				back();
+				moveWall();
 				if (direction.equals("Wleft")) {
 					direction = "Wright";
 				}
@@ -199,16 +240,14 @@ public class CharMove {
 			}
 		}).start();
 		
-		
-		
 	}
 	
 	public void moveWaterLeft() {
 		left = true;
 		new Thread(()->{
 			while(pos_X>10&&left) {
-				map();
-				//block();
+				back();
+				moveWall();
 				if (direction.equals("Wright")) {
 					direction = "Wleft";
 				}
@@ -258,6 +297,7 @@ public class CharMove {
 			
 		}).start();
 	}
+	
 	public void up2() {
 		up = true;
 		new Thread(()->{
@@ -297,25 +337,35 @@ public class CharMove {
 		down = true;
 		new Thread(()->{
 			for(int i =0; i<80/JUMPSPEED; i++) {
+				//첫번째 오른쪽 발판
 				if(pos_X>=830&&pos_X<=910&&pos_Y<=580&&pos_Y>=560) break;
-				
+				//두 번째줄 좌표
 				if(pos_Y>=490&&pos_Y<=492&&pos_X<830&&pos_X>390) break;
-				
+				//두번째줄 버튼있는 길 좌표
 				if(pos_Y>=432&&pos_Y<=436&&pos_X<=390&&pos_X>10) break;
+				//움직이는 벽 좌표
+				if(pos_X<150&&pos_Y>=435-45&&pos_Y<=435-45+2&&Block.getpush()==true) break;
 				
-				if(pos_X<150&&pos_Y>=435-29-29&&pos_Y<=435-29-29+2) break;
-				
-				if(pos_Y>=308&pos_Y<=310&&pos_X<=110&&pos_X>10) break;
-				
+				//세 번째줄 구간 좌표
+				if(pos_Y>=312&pos_Y<=310&&pos_X<=460&&pos_X>10) break;
+				//세 번째 구간 내려간 후 좌표
 				if(pos_Y>=328&pos_Y<=330&&pos_X<=910&&pos_X>500) break;
+				//세 번째줄 오른쪽 돌 좌표
+				if(pos_Y>=275&&pos_Y<=330&&pos_X<=910&&pos_X>850) break;
+				//네 번째줄 좌표
+				if(pos_Y>=205&&pos_Y<=208&&pos_X<=823&&pos_X>700) break;
+				//네 번째줄(2) 좌표
+				if(pos_Y>=153&&pos_Y<=155&&pos_X<=700&&pos_X>=475) break;
+				//네 번째줄 돌 좌표
+				if(pos_Y>=150&&pos_Y<=152&&pos_X<=180&pos_X>=120) break;
+				//네 번째줄(3) 좌표
+				if(pos_Y>=100&&pos_Y<=102&&pos_X<=120&pos_X>=10) break;
+				//다섯 번째줄 좌표
+				if(pos_Y>=48&&pos_Y<=50&&pos_X<250&pos_X>=210) break;
+				if(pos_Y>=25&&pos_Y<=27&&pos_X<=295&pos_X>=226) break;
 				
-				if(pos_Y==75&&pos_X<=910&&pos_X>500) break;
 				
 				pos_Y+=JUMPSPEED;
-				
-				
-			
-				
 				
 				try {
 					Thread.sleep(18);
@@ -326,12 +376,13 @@ public class CharMove {
 			down=false;
 		}).start();
 		
-	}	
+	}
+	
 	public void down2() { //캐릭터 다운
 			
 		down = true;
 		new Thread(()->{
-			for(int i =0; i<100/JUMPSPEED; i++) {
+			for(int i =0; i<JUMPDOWN/JUMPSPEED; i++) {
 				
 				pos_Y+=JUMPSPEED;
 				try {
@@ -344,23 +395,8 @@ public class CharMove {
 		}).start();
 		
 	}	
-	public void down3() { //캐릭터 다운
-		
-		down = true;
-		new Thread(()->{
-			for(int i =0; i<25/JUMPSPEED; i++) {
-				
-				pos_Y+=JUMPSPEED;
-				try {
-					Thread.sleep(17);
-				}catch(InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			down=false;
-		}).start();
-		
-	}	
+	
+	
 			
 //		if (direction.equals("left") && KeyListener.pressedLeftKey) {
 //			pos_X -= 100;
